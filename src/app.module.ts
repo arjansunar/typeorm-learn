cd435ea08d0ee4d826c33cee44aeb4970cf28712
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-03-27 16:50:32 +0545

    creating typeorm entity and user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogModule } from './dog/dog.module';
-import { DatabaseModule } from './database/database.module';
+// import { DatabaseModule } from './database/database.module';
+// import { config } from './ormconfig';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DogModule, DatabaseModule],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+import { MysqlConnectionCredentialsOptions } from 'typeorm/driver/mysql/MysqlConnectionCredentialsOptions';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+// import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
