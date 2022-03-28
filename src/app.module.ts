ec8e3ada48155095ae6c42ce1595481f9868b646
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-03-28 12:59:12 +0545

    removing spec files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,9 @@ import { DogModule } from './dog/dog.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db',
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'nest',
+      type: 'sqlite',
+      database: 'db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
--- src/dog/dog.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DogEntity } from './dog.entity';
-
-describe('DogEntity', () => {
-  it('should be defined', () => {
-    expect(new DogEntity()).toBeDefined();
-  });
-});
